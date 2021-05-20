9d096f0719d881d10eab1d84ff7f7e10cde0abfa
Epos95 <39768937+Epos95@users.noreply.github.com>
2021-05-20 13:54:01 +0200

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// maybe treat 15 minutes as the smallest unit of time, then the user can specify how many
+// of these they want to use in the schedule and makes drawing the schedule easier
 fn main() {
-    println!("Hello, world!");
 }
